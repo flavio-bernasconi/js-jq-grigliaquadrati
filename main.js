57398a3ed2f91f77f70bfb69c8a05d7402a13ccd
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-17 13:11:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+
+  $("div.rosso").click(function(){
+    $(this).css("background","red");
+  });
+
+
+  $("div.verde").click(function(){
+    $(this).css("background","green");
+  });
+
+
+
+
+
+
+
+});
